e82d7a45863127590f7f1a84e893fd4f036a48f6
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-10-31 23:07:09 +0100

    Initial Commit

---
+++ Shared/MapRoutesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapRoutesApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 31/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MapRoutesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
